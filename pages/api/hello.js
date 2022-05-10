f0bda227253bd3af19955a423cf37c46ba554bee
rezashokri99 <rezashokri.js@gmail.com>
2022-05-10 16:41:34 +0430

    build server.js file and util directory and .env file

---
+++ pages/api/hello.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default (req, res) => {
+    res.status(200).json({name: "reza shokri"})
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,23 @@
 import Main from "../components/main";
 
 export default function Home() {
-  
+
   return (
     <div>
-      
+
       {/* main container */}
       <Main />
-      
+
     </div>
   )
+}
+
+export const getServerSideProps = async (context) => {
+
+  return {
+    props: {
+      
+    }
+  }
+
 }
